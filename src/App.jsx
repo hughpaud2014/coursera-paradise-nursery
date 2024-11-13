ab1adcbf003bd67a02a0d7755c99d7c2826237f0
Paul Hughes <49007269+hughpaud2014@users.noreply.github.com>
2024-11-12 23:41:36 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
+      <Router basename="corsera-paradise-nursery">
         <CartProvider>
           <Routes>
             <Route path="/" element={<LandingPage />} />
